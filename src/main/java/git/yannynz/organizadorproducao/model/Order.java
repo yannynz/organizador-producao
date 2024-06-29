b23dab8d34de7b1c906adbe237993601103e8217
yannynz <testeyann@hotmail.com>
2024-06-29 18:18:14 -0300

    backEnd1v

--- src/main/java/git/yannynz/organizadorproducao/model/Order.java
+++ src/main/java/git/yannynz/organizadorproducao/model/Order.java
@@ -1,13 +1,11 @@
 package git.yannynz.organizadorproducao.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
