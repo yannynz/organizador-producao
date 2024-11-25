435c5ae5ae709c2f2f226a42101bda3936b442fa
yannynz <testeyann@hotmail.com>
2024-11-25 13:22:34 -0300

    beta2.5Prod

--- src/main/java/git/yannynz/organizadorproducao/config/WebConfig.java
+++ src/main/java/git/yannynz/organizadorproducao/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost", "http://192.168.0.101", "http://nginx-container", "http://frontend-container/") // Permitir requisições de qualquer origem
+                .allowedOrigins("http://localhost", "http://192.168.0.101", "http://nginx-container:80", "http://frontend-container/") // Permitir requisições de qualquer origem
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Permitir credenciais, se necessário
