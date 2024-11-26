fb65b30885cd2e903ebfd87906983b1bf7180b8b
yannynz <testeyann@hotmail.com>
2024-11-26 10:26:50 -0300

    beta2.5prodV3

--- organizer-front/src/app/enviroment.ts
+++ organizer-front/src/app/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.10.37:8080/api/orders', // URL do backend em desenvolvimento
-  wsUrl: 'ws://192.168.10.37:8080/ws/orders', // URL do WebSocket
+  apiUrl: 'http://192.168.10.28:8080/api/orders', // URL do backend em desenvolvimento
+  wsUrl: 'ws://192.168.10.28:8080/ws/orders', // URL do WebSocket
 };
