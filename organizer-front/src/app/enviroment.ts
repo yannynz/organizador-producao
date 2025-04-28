04600492340f21f293850920d998d110edfc2a23
yannynz <testeyann@hotmail.com>
2025-04-28 11:53:29 -0300

    1.2.3Prod

--- organizer-front/src/app/enviroment.ts
+++ organizer-front/src/app/enviroment.ts
@@ -1,7 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://192.168.10.13:8080/api/orders', // URL do backend
- //apiUrl: 'http://192.168.0.116:8080/api/orders', // URL do backend em desenvolvimento
-  wsUrl: 'ws://192.168.10.13:8080/ws/orders', // URL do WebSocket
- //wsUrl: 'ws://192.168.0.116:8080/ws/orders', // URL do WebSocket em desenvolvimento
+  production: true,
+  apiUrl: '/api/orders',  // -> apenas caminho relativo, porque o nginx redireciona
+  wsUrl: 'ws://' + window.location.host + '/ws/orders',  // -> WebSocket relativo ao host onde a página foi carregada
 };
+
