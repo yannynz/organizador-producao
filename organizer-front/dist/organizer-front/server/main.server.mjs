cdf7785d19e0437411b74a87bf8cb94d0a9ce5d5
yannynz <testeyann@hotmail.com>
2025-12-04 17:49:07 -0300

    Versão 0.7.5

--- organizer-front/dist/organizer-front/server/main.server.mjs
+++ organizer-front/dist/organizer-front/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BQTHH2U2.mjs";import"./chunk-ONQ3GI3P.mjs";import"./chunk-4XF6TFS5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-6EEU7OUF.mjs";import"./chunk-ONQ3GI3P.mjs";import"./chunk-4XF6TFS5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
