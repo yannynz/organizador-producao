978b2237ed9c3524106e171d35c6fa6ae6ea163d
yannynz <testeyann@hotmail.com>
2026-02-03 08:33:35 -0300

    Versão 1.2.6

--- organizer-front/dist/organizer-front/server/main.server.mjs
+++ organizer-front/dist/organizer-front/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TESIJPZ3.mjs";import"./chunk-O2LYCPXN.mjs";import"./chunk-QPM3NXUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-FK6X7EN5.mjs";import"./chunk-K5GUQQSZ.mjs";import"./chunk-YGW533LA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
